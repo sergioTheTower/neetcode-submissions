299763979596a71a58fc5533d53bf54e35a86570
Sergio <200695463+sergioTheTower@users.noreply.github.com>
2026-04-29 12:58:45 -0600

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.go
@@ -0,0 +1,16 @@
+func groupAnagrams(strs []string) [][]string {
+	groups := map[[26]int][]string{}
+	for _, word := range strs {
+		key := [26]int{}
+		for _, r := range word {
+			key[r-'a']++
+		}
+		groups[key] = append(groups[key], word)
+		fmt.Println(key)
+	}
+	format := [][]string{}
+	for _, arr := range groups{
+		format = append(format, arr)
+	}
+	return format
+}
